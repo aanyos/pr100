21216be7940787e993ba4c8adb56764a39209213
aanyos <85759900+aanyos@users.noreply.github.com>
2022-06-27 20:41:28 -0600

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,32 @@
+// Your web app's Firebase configuration
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+    apiKey: "AIzaSyCoLpTfNokpeMI_oc1BafZmY62gE-03K3w",
+    authDomain: "project-101-5e7d1.firebaseapp.com",
+    databaseURL: "https://project-101-5e7d1-default-rtdb.firebaseio.com",
+    projectId: "project-101-5e7d1",
+    storageBucket: "project-101-5e7d1.appspot.com",
+    messagingSenderId: "417912322344",
+    appId: "1:417912322344:web:75734212990e0728f7b81d"
+  };
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+function addUser()
+{
+  user_name = document.getElementById("user_name").value;
+  firebase.database().ref("/").child(user_name).update({
+    purpose : "adding user"
+  });
+
+localStorage.setItem("user_name", user_name);
+
+
+// with windows.location uncommented add use to firebase won't work
+
+//window.location = "chat_room.html";
+}
+
+
